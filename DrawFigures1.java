1e31749e6e57ebc0641fb4d19dc6542194c9d550
abeeku <kwekubondzie@gmail.com>
2014-09-16 10:28:10 -0400

    Add Procedural Decomp

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,7 +1,21 @@
 public class DrawFigures1 {
     public static void main(String[] args) {
 	// They all need to be put in seperate methods for programs decomposition
-        System.out.println("   /\\");
+		draw_tri_up();
+		draw_tri_down();
+		System.out.println();
+		draw_tri_down();
+		draw_tri_up();
+		System.out.println();
+		draw_tri_up();
+		
+		draw_US_flag();
+		
+		draw_tri_up();
+		
+		
+		
+ /*       System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
         System.out.println(" \\    /");
@@ -30,6 +44,33 @@ public class DrawFigures1 {
         System.out.println("+------+");
         System.out.println("   /\\");
         System.out.println("  /  \\");
-        System.out.println(" /    \\");
+        System.out.println(" /    \\"); 
+		*/
     }
+	
+	public static void draw_tri_up() {
+		System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+	}
+	
+	public static void draw_tri_down() {
+		System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+	}
+	
+	public static void draw_US_flag() {
+		System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+	}
+		
 }
